2825ef12d9acd690d7a418c08b78609a22a2594f
riveur <riveur.cpy@gmail.com>
2024-11-25 15:32:34 +0100

    feat: setup shadcn/ui

---
+++ app/components/app-sidebar.tsx
@@ -0,0 +1,73 @@
+import { FileTextIcon } from "lucide-react"
+import * as React from "react"
+import { Link } from "react-router"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "~/components/ui/sidebar"
+
+const items = [
+  {
+    title: "Application",
+    url: "#",
+    items: [
+      {
+        title: "Users",
+        url: "/users",
+      },
+      {
+        title: "Posts",
+        url: "/posts",
+      },
+    ],
+  },
+]
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link to="/">
+                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-destructive text-sidebar-primary-foreground">
+                  <FileTextIcon className="size-4" />
+                </div>
+                  <span className="font-semibold">RR7 Blog</span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        {items.map((item) => (
+          <SidebarGroup key={item.title}>
+            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {item.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild isActive={false}>
+                      <a href={item.url}>{item.title}</a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,11 @@
-import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  ...prefix('/users', [
-    index('routes/users/index.tsx'),
-    route(':id', 'routes/users/show.tsx')
+  layout('components/layout.tsx', [
+    index("routes/home.tsx"),
+    ...prefix('/users', [
+      index('routes/users/index.tsx'),
+      route(':id', 'routes/users/show.tsx')
+    ])
   ])
 ] satisfies RouteConfig;
